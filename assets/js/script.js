03a03a96d751ae270e2dcce4019c1ca1949a6b5d
Przybeuka <jeander.kauanp@gmail.com>
2025-02-21 20:30:13 -0300

    Implementation photos gallery

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,3 +59,26 @@ fetch('data.json')
     })
     .catch(err => console.error('Erro ao carregar data.json:', err));
 
+// Caso use esse Swiper para galeria
+var swiperGallery = new Swiper('.mySwiperGallery', {
+    slidesPerView: 2,
+    spaceBetween: 10,
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    // Responsivo
+    breakpoints: {
+        768: {
+            slidesPerView: 3
+        },
+        1024: {
+            slidesPerView: 4
+        }
+    }
+});
